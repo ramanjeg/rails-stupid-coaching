d8e2efd5d13db20aaeee7ad9236ea2051abc8ff8
Raman Jeganathan <raman.jeg@gmail.com>
2023-05-22 15:43:15 +0200

    Q&A working

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:prompt]
+
+    if @question == "I am going to work"
+      @response = "Great"
+    elsif @question.end_with?("?")
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+end
